6d4b451881af4a312b0deb22b6e8e094b31c44ef
wrayta <wrayta@hotmail.com>
2016-12-22 12:57:58 -0500

    Added directions in the form of a comment

--- String-3countYZ.java
+++ String-3countYZ.java
@@ -1,3 +1,14 @@
+/*
+ * Given a string, count the number of words ending in 'y' or 'z' -- so the 'y' 
+ * in "heavy" and the 'z' in "fez" count, but not the 'y' in "yellow" (not case
+ * sensitive). We'll say that a y or z is at the end of a word if there is not 
+ * an alphabetic letter immediately following it. (Note: Character.isLetter(char)
+ * tests if a char is an alphabetic letter.)
+ * 
+ * countYZ("fez day") → 2
+ * countYZ("day fez") → 2
+ * countYZ("day fyyyz") → 2
+ */
 public int countYZ(String str) {
   int count = 0;
   String[] words = str.split("\\s+");
@@ -11,7 +22,7 @@ public int countYZ(String str) {
             || (words[i].charAt(j) == 'z') 
             || (words[i].charAt(j) == 'Z')){
               count++;
-            }
+          }
         }
       }
       else if( (words[i].charAt(j) == 'y') 
@@ -19,7 +30,7 @@ public int countYZ(String str) {
             || (words[i].charAt(j) == 'z') 
             || (words[i].charAt(j) == 'Z')){
               count++;
-            }
+      }
     }
   }
   
