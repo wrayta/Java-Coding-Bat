2fa45ac7c99195f27330fac000f4727fedbb72b8
wrayta <wrayta@hotmail.com>
2017-12-02 00:14:22 -0500

    Completed BunnyEars 2

---
+++ Recursion-1bunnyEars2.java
@@ -0,0 +1,29 @@
+/*
+We have bunnies standing in a line, numbered 1, 2, ... The odd 
+bunnies (1, 3, ..) have the normal 2 ears. The even bunnies 
+(2, 4, ..) we'll say have 3 ears, because they each have a raised 
+foot. Recursively return the number of "ears" in the bunny line 
+1, 2, ... n (without loops or multiplication).
+
+bunnyEars2(0) → 0
+bunnyEars2(1) → 2
+bunnyEars2(2) → 5
+*/
+
+public int bunnyEars2(int bunnies) {
+  if(bunnies == 0) {
+    return 0;
+  } else if(bunnies % 2 == 0) { // even
+    if(bunnies == 1) {
+      return 3;
+    } else {
+      return 3 + bunnyEars2(bunnies - 1);
+    }
+  } else { // odd
+     if(bunnies == 1) {
+       return 2;
+     } else {
+       return 2 + bunnyEars2(bunnies - 1);
+     }
+  } 
+}
